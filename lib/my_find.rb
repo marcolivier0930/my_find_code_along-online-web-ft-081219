30cdff842b0c9639257ba90d965bbb06cc6160bd
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-19 02:07:38 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-  return yield(collection[i])
-  return collection[i]
-  i += 1 
+    return collection[i] if 
+    yield(collection[i])
+    i += 1
+  end
   
 end
